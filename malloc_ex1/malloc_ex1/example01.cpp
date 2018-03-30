df368e9aa06f3b702b49c9e12711ea1b06efda2a
aLIEz2476 <sakuragun11@gmail.com>
2018-03-30 15:31:13 +0900

    1

--- malloc_ex1/malloc_ex1/example01.cpp
+++ malloc_ex1/malloc_ex1/example01.cpp
@@ -1,18 +1,66 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
-int main()
+
+struct GameObject {
+	char name[100];
+	char tag[100];
+	bool ActiveSelf;
+};
+
+void StructMain()
+{
+	GameObject sGameObject;
+	strcpy_s(sGameObject.name, "Cube");
+	strcpy_s(sGameObject.tag, "Player");
+	sGameObject.ActiveSelf = true;
+
+	GameObject arrGameObject[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		sprintf(arrGameObject[i].name, "Cube %d", i);
+		strcpy_s(arrGameObject[i].name, "Player");
+		arrGameObject[i].ActiveSelf = true;
+
+		printf("GameObject[%d]:%s\n", i, arrGameObject[i].name);
+
+	}
+
+	GameObject* pGameObject = (GameObject*)malloc(sizeof(GameObject));
+	strcpy_s(pGameObject->name, "Cube");
+	strcpy_s(pGameObject->tag, "Player");
+	pGameObject->ActiveSelf = true;
+	
+	GameObject sB = sGameObject; // sGameObject의 데이터가 sB로 넘어가게 되는 memcpy 발생
+
+	free(pGameObject);
+	// strcpy시 동적할당을 시켰기 때문에 -> 화살표 반드시 필요
+
+}
+
+void DynamicMain()
 {
 	int nSize = 0;
 	scanf("%d", &nSize);
-	int* pDynamic = (int*)malloc(nSize * sizeof(int)); // 100칸의 공간 부여
+	int* pDynamic = new int[nSize]; //(int*)malloc(nSize * sizeof(int)) 과 기능 같음
 	
+
 	for (int i = 0; i < 100; i++)
 	{
-		*(pDynamic + i) = i+1;
+		*(pDynamic + i) = i + 1;
 		printf("%d\n", *(pDynamic + i));
 	} // 공간 부여된 포인터에 데이터 입력
 	printf("\n");
-	free(pDynamic); // 부여된 공간을 반환
+	delete[] pDynamic; // pDynamic을 배열로 선언했을 경우, delete 사용시 배열임을 알리기 위해 []을 붙임
+	pDynamic = new int();
+	delete pDynamic; // pDyanmic을 단일로 선언했다면, delete 걍 쓰면 됨
+	//free(pDynamic);  부여된 공간을 반환
+}
+
+int main()
+{
+	StructMain();
 }
 
 // 빌드순서 : 코드작성 -> 컴파일러(문법 검사) -> 실행파일 생성
